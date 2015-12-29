2dc34a8c97c9013555fe9118d51d8467b2d6573f
GGCrew <R_Connelie@hotmail.com>
2015-12-29 10:56:25 -0800

    Fleshing out code to parse hero rosters.  Currently extracting correct chunks of text; still need to refine and process the texts.

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -5,9 +5,27 @@ class Roster < ActiveRecord::Base
 
 	def self.update_from_blizzard
 		address = 'http://us.battle.net/heroes/en/forum/topic/17936383460'
+		date_search_text = 'Free-to-Play Hero Rotation:'
+
 		url = URI.parse(address)
 		html = Net::HTTP.get(url) # TODO: error handling
 		page = Nokogiri::HTML(html)
+
+		date_search_regex = Regexp.new(Regexp.quote(date_search_text))
+
+		post_list = page.css('div.post-list div.topic-post')
+		date_text = nil
+		for post in post_list[0..0]  # TEMPORARY CODE: range restriction during development
+			post_detail = post.css('div.post-detail')
+			post_detail.first.traverse do |node|
+				if node.text?
+					date_text = node.text if (date_search_regex =~ node.text)
+				end
+			end
+			hero_texts = post_detail.css('ul li').map{|i| i.text}
+		end
+		
+		return {post_list_count: post_list.count, date_text: date_text, hero_texts: hero_texts}
 	end
 
 end
