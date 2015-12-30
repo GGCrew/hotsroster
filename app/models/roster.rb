d3459e0ffad4f4375c559481002cc320e50592f3
= <=>
2015-12-30 12:59:56 -0800

    Parsing of weekly roster nearly complete.  Still need to check for pagination on the Blizzard forums (spoiler: it needs to be done), and actually import the parsed data into the database.

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -12,10 +12,9 @@ class Roster < ActiveRecord::Base
 		page = Nokogiri::HTML(html)
 
 		date_search_regex = Regexp.new(Regexp.quote(date_search_text))
-		date_texts = []	# TEMPORARY CODE
 	
 		post_list = page.css('div.post-list div.topic-post')
-		for post in post_list  #[0..0]  # TEMPORARY CODE: range restriction during development
+		for post in post_list
 			date_text = nil
 			post_detail = post.css('div.post-detail')
 			post_detail.first.traverse do |node|
@@ -23,19 +22,40 @@ class Roster < ActiveRecord::Base
 					date_text = node.text.dup if (date_search_regex =~ node.text)
 				end
 			end
-			hero_texts = post_detail.css('ul li').map{|i| i.text}
+			hero_texts = post_detail.css('ul li').map{|i| i.text.strip}
 			
 			# parse dates into DateRange
 			date_text.gsub!(date_search_text, '')
 			date_text.strip!
 			date_range = DateRange.import_date_text(date_text)
 
-			# TODO: parse heroes and player levels
+			# parse heroes and player levels
+			# Note: I spent time developing a RegEx to check for the presense or absense
+			#       of the "(Slot unlocked...)" text, but it was becoming overly complex.
+			#       Only checking for the presense of the text is much clearer,
+			#       as is a simple if-else for handling the RegEx match results.
+			heroes = []
+			for hero_text in hero_texts
+				hero_match = /^(.*) \(Slot unlocked at Player Level (\d{1,2})\)$/.match(hero_text)
+				if hero_match
+					# hero requires a player level
+					heroes << {
+						name: hero_match[1],
+						player_level: hero_match[2].to_i
+					}
+				else
+					# hero does not require a player level
+					heroes << {
+						name: hero_text,
+						player_level: 1
+					}
+				end
+			end
 
 			# TODO: Import into Roster
 		end
 		
-		return {post_list_count: post_list.count, date_range: date_range, hero_texts: hero_texts}
+		return {date_range: date_range, heroes: heroes}
 	end
 
 end
