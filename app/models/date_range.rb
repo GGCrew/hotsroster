fe381fd16a2dc6b72a95e30e4fc83a934de6825e
= <=>
2015-12-30 01:25:11 -0800

    Renamed assorted "update..." methods to "import..." Successful parsing of date text.

--- app/models/date_range.rb
+++ app/models/date_range.rb
@@ -2,4 +2,28 @@ class DateRange < ActiveRecord::Base
 
 	has_many	:rosters
 
+	def self.import_date_text(date_text)
+		date_match = /^([a-zA-Z]*) (\d{1,2}) . ([a-zA-Z]*) ?(\d{1,2}), (\d{4})$/.match(date_text)
+		start_month = date_match[1]
+		start_day = date_match[2]
+		end_month = date_match[3]
+		end_day = date_match[4]
+		year = date_match[5]
+		start_date = DateTime.parse("#{start_month} #{start_day}, #{year}")
+		(end_month = start_month) if (end_month.blank? && (end_day.to_i == (start_day.to_i + 7)))
+		end_date = DateTime.parse("#{end_month} #{end_day}, #{year}")
+		
+		# Kludge for funky end-of-year date text like "Dec 29 - Jan 5, 2015"
+		if start_date > end_date
+			if ((start_date + 1.week).month == end_date.month) && ((start_date + 1.week).day == end_date.day)
+				end_date = start_date + 1.week
+			end
+		end
+
+		date_range = self.where(start: start_date).where(end: end_date).first
+		date_range = self.create!(start: start_date, end: end_date) unless date_range
+
+		return date_range
+	end		
+	
 end
--- app/models/typp.rb
+++ app/models/typp.rb
@@ -2,7 +2,7 @@ class Typp < ActiveRecord::Base
 
 	has_many	:heroes
 
-	def self.update_from_json(json)
+	def self.import_from_json(json)
 		for typp in json
 			self.create!(name: typp['name'], slug: typp['slug']) unless self.where(name: typp['name']).first
 		end
