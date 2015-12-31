708b15f9b0c019c8823d551fe6b33cfe1985ed9c
GGCrew <R_Connelie@hotmail.com>
2015-12-31 13:30:11 -0800

    Added fixture data for testing.

--- test/controllers/rosters_controller_test.rb
+++ test/controllers/rosters_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class RostersControllerTest < ActionController::TestCase
   setup do
-    @roster = rosters(:one)
+    @roster = rosters(:roster_1)
   end
 
   test "should get index" do
