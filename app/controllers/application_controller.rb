1cd6c4119a9b2d3b381fe8530b122fe3ad33ae55
GGCrew <R_Connelie@hotmail.com>
2016-01-09 10:36:40 -0800

    Replaced static head title and meta description with dynamic entries.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter	:define_head_tags
+
+	def define_head_tags
+		@head = HashWithIndifferentAccess.new
+		@head.merge!(title: nil)
+		@head.merge!(meta: HashWithIndifferentAccess.new)
+		@head[:meta].merge!(description: nil)
+	end
 end
