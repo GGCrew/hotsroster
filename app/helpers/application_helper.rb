4762dc0996d420de4eaee5e1d3c677ce81adb427
= <=>
2016-01-09 14:06:00 -0800

    Adding Hero index. Moved rotation calculations into application_helper.  Cleaner views, and reusable code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+
+	def rotations_since_launch(hero)
+		hero.date_ranges.count
+	end
+
+	def rotation_percentage_since_launch(hero)
+		(hero.date_ranges.count / DateRange.count.to_f * 100).round(2)
+	end
+
+	def rotations_since_release(hero)
+		conditions = ['start >= :hero_date', {hero_date: hero.release_date}]
+		hero.date_ranges.where(conditions).count
+	end
+
+	def rotation_percentage_since_release(hero)
+		conditions = ['start >= :hero_date', {hero_date: hero.release_date}]
+		(hero.date_ranges.where(conditions).count / DateRange.where(conditions).count.to_f * 100).round(2)
+	end
+
+	def rotations_since_newest_hero_release(hero)
+		conditions = ['start >= :hero_date', {hero_date: Hero.newest.release_date}]
+		hero.date_ranges.where(conditions).count
+	end
+
+	def rotation_percentage_since_newest_hero_release(hero)
+		conditions = ['start >= :hero_date', {hero_date: Hero.newest.release_date}]
+		(hero.date_ranges.where(conditions).count / DateRange.where(conditions).count.to_f * 100).round(2)
+	end
+
 end
