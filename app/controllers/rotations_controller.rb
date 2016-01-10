ad18fb03d9273e92d45144d6efca727a8b9d0bde
GGCrew <R_Connelie@hotmail.com>
2016-01-10 00:18:50 -0800

    Adding head messages.

--- app/controllers/rotations_controller.rb
+++ app/controllers/rotations_controller.rb
@@ -1,6 +1,9 @@
 class RotationsController < ApplicationController
 
   def index
+		@head[:meta][:description] = "Hero Rotations for Blizzard's Heroes of the Storm."
+		@head[:title] = "Rotations"
+
     @rotations = DateRange.order(end: :desc).order(start: :desc)
   end
 
