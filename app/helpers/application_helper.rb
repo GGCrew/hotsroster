796a6b1d167e5f939e838e21e65f8d7cd3667a9c
GGCrew <R_Connelie@hotmail.com>
2016-01-11 03:02:50 -0800

    Added method and helpers to compute rotations since latest change in roster size. (Inspired by looking at the Rotations#index page and clearly seeing the increased roster size.)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,23 +8,33 @@ module ApplicationHelper
 		(hero.date_ranges.count / DateRange.count.to_f * 100).round(2)
 	end
 
+	def rotations_since_latest_change_in_roster_size(hero)
+		conditions = ['start >= :roster_date', {roster_date: Roster.date_of_latest_roster_size_change.start}]
+		hero.date_ranges.where(conditions).count
+	end
+
+	def rotation_percentage_since_latest_change_in_roster_size(hero)
+		conditions = ['start >= :roster_date', {roster_date: Roster.date_of_latest_roster_size_change.start}]
+		(hero.date_ranges.where(conditions).count / DateRange.where(conditions).count.to_f * 100).round(2)
+	end
+
 	def rotations_since_release(hero)
-		conditions = ['start >= :hero_date', {hero_date: hero.release_date}]
+		conditions = ['start >= :release_date', {release_date: hero.release_date}]
 		hero.date_ranges.where(conditions).count
 	end
 
 	def rotation_percentage_since_release(hero)
-		conditions = ['start >= :hero_date', {hero_date: hero.release_date}]
+		conditions = ['start >= :release_date', {release_date: hero.release_date}]
 		(hero.date_ranges.where(conditions).count / DateRange.where(conditions).count.to_f * 100).round(2)
 	end
 
 	def rotations_since_newest_hero_release(hero)
-		conditions = ['start >= :hero_date', {hero_date: Hero.newest.release_date}]
+		conditions = ['start >= :newest_hero_date', {newest_hero_date: Hero.newest.release_date}]
 		hero.date_ranges.where(conditions).count
 	end
 
 	def rotation_percentage_since_newest_hero_release(hero)
-		conditions = ['start >= :hero_date', {hero_date: Hero.newest.release_date}]
+		conditions = ['start >= :newest_hero_date', {newest_hero_date: Hero.newest.release_date}]
 		(hero.date_ranges.where(conditions).count / DateRange.where(conditions).count.to_f * 100).round(2)
 	end
 
