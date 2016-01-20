87cf4ee8d3c89a8f315b12e5264886c49b00b1f6
GGCrew <R_Connelie@hotmail.com>
2016-01-19 23:59:44 -0800

    Added "hero not found" email. Fleshed out Mailer Preview. Deleted old test files.

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,11 +1,6 @@
 class AdminMailer < ApplicationMailer
 	@@admin = ENV['SITE_ADMIN']
 	
-	def test
-		AdminMailer.set_smtp_settings
-		mail(to: @@admin, subject: 'Test')
-	end
-
 	def roster_unrecognized_hero_name(hero_name, date_range)
 		@hero_name = hero_name
 		@date_range = AdminMailer.format_date_range(date_range)
@@ -19,5 +14,18 @@ class AdminMailer < ApplicationMailer
 		AdminMailer.set_smtp_settings
 		mail(to: @@admin,  subject: subject)
 	end
-	
+
+	def roster_hero_not_found(hero_text, date_range)
+		@hero_text = hero_text
+		@date_range = AdminMailer.format_date_range(date_range)
+		
+		subject = [
+			@date_range,
+			'Hero Not Found'
+		].join(' - ')
+		
+		AdminMailer.set_smtp_settings
+		mail(to: @@admin,  subject: subject)
+	end
+
 end
--- test/mailers/previews/admin_mailer_preview.rb
+++ test/mailers/previews/admin_mailer_preview.rb
@@ -1,4 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/admin_mailer
 class AdminMailerPreview < ActionMailer::Preview
 
+	def roster_unrecognized_hero_name_preview
+		AdminMailer.roster_unrecognized_hero_name('Blarg', DateRange.last)
+	end
+
+	def roster_hero_not_found
+		AdminMailer.roster_hero_not_found('Blarg (Slot unlocked at Player Level 5)', DateRange.last)
+	end
+
 end
