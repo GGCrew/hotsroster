abcae77ad9bb573219addf7f452611980b4e2c4f
GGCrew <R_Connelie@hotmail.com>
2016-01-21 15:49:39 -0800

    Tests now checking all validation rules for Hero model. Tests now checking all validation rules for Franchise model.

--- test/models/franchise_test.rb
+++ test/models/franchise_test.rb
@@ -6,6 +6,11 @@ class FranchiseTest < ActiveSupport::TestCase
 		value: 'Franchise Value'
 	}
 
+	bogus_attributes = {
+		name: 'Bogus Franchise Name',
+		value: 'Bogus Franchise Value'
+	}
+	
 	test 'should not save franchise without name' do
 		franchise = Franchise.new attributes.reject{|k,v| k == :name}
 		assert_not franchise.save
@@ -13,6 +18,18 @@ class FranchiseTest < ActiveSupport::TestCase
 
 	test 'should not save franchise without value' do
 		franchise = Franchise.new attributes.reject{|k,v| k == :value}
+		assert_not franchise.save
+	end
+
+	test 'should not save franchise with duplicate name' do
+		Franchise.create bogus_attributes.merge(name: attributes[:name])
+		franchise = Franchise.new attributes
+		assert_not franchise.save
+	end
+
+	test 'should not save franchise with duplicate value' do
+		Franchise.create bogus_attributes.merge(value: attributes[:value])
+		franchise = Franchise.new attributes
 		assert_not franchise.save
 	end
 
--- test/models/hero_test.rb
+++ test/models/hero_test.rb
@@ -4,19 +4,53 @@ class HeroTest < ActiveSupport::TestCase
 	attributes = {
 		name: 'Hero Name',
 		slug: 'hero-slug',
-		franchise: Franchise.create!(name: 'Hero Franchise Name', value: 'hero_franchise_value'),
-		role: Role.create!(name: 'Hero Role Name', slug: 'hero-role-slug'),
-		typp: Typp.create!(name: 'Hero Typp Name', slug: 'hero-typp-slug')
+		franchise: Franchise.find_or_create_by(name: 'Hero Franchise Name', value: 'hero_franchise_value'),
+		role: Role.find_or_create_by(name: 'Hero Role Name', slug: 'hero-role-slug'),
+		typp: Typp.find_or_create_by(name: 'Hero Typp Name', slug: 'hero-typp-slug')
+	}
+
+	bogus_attributes = {
+		name: 'Bogus Hero Name',
+		slug: 'bogus-hero-slug',
+		franchise: attributes[:franchise],
+		role: attributes[:role],
+		typp: attributes[:typp]
 	}
 
 	test "should not save hero without name" do
-		p attributes
 		hero = Hero.new attributes.reject{|k,v| k == :name}
 		assert_not hero.save
 	end
 
 	test "should not save hero without slug" do
 		hero = Hero.new attributes.reject{|k,v| k == :slug}
+		assert_not hero.save
+	end
+
+	test "should not save hero without franchise" do
+		hero = Hero.new attributes.reject{|k,v| k == :franchise}
+		assert_not hero.save
+	end
+
+	test "should not save hero without role" do
+		hero = Hero.new attributes.reject{|k,v| k == :role}
+		assert_not hero.save
+	end
+
+	test "should not save hero without typp" do
+		hero = Hero.new attributes.reject{|k,v| k == :typp}
+		assert_not hero.save
+	end
+
+	test "show not save hero with duplicate name" do
+		Hero.create! bogus_attributes.merge(name: attributes[:name])
+		hero = Hero.new attributes
+		assert_not hero.save
+	end
+
+	test "show not save hero with duplicate slug" do
+		Hero.create! bogus_attributes.merge(slug: attributes[:slug])
+		hero = Hero.new attributes
 		assert_not hero.save
 	end
 
