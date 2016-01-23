82d2ab0d77cd6e35152461eac887fdf445f92fc9
GGCrew <R_Connelie@hotmail.com>
2016-01-23 15:43:09 -0800

    Added controller tests for Home.  Also testing that certain formats DO NOT process. Added controller tests for Heros.  Also testing that certain actions DO NOT process.  Using this to help keep site secure by disabling unused actions.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,4 +5,26 @@ class HomeControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get copyrights" do
+    get :copyrights
+    assert_response :success
+  end
+
+  test "should get sitemap.xml" do
+    get :sitemap, format: :xml
+    assert_response :success
+    assert_raises(Exception) { get :sitemap }
+  end
+
+  test "should get robots.txt" do
+    get :robots, format: :txt
+    assert_response :success
+    assert_raises(Exception) { get :robots }
+  end
 end
