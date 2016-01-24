c546c7a5c0189691cf399c602e844f678da182d9
GGCrew <R_Connelie@hotmail.com>
2016-01-23 22:08:32 -0800

    Query tweaks to avoid some 1+N situations.

--- app/controllers/rotations_controller.rb
+++ app/controllers/rotations_controller.rb
@@ -5,14 +5,15 @@ class RotationsController < ApplicationController
 		@head[:meta][:description] = "Hero Rotations for Blizzard's Heroes of the Storm."
 		@head[:title] = "Rotations"
 
-    @rotations = DateRange.order(end: :desc).order(start: :desc)
+    #@rotations = DateRange.order(end: :desc).order(start: :desc)
+    @rotations = DateRange.order(end: :desc).order(start: :desc).includes(:rosters).order('rosters.player_level ASC').includes(:heroes).order('heros.name ASC')
   end
 
 	def show
 		@head[:meta][:description] = "#{@rotation.start.to_s(:shortish)} - #{@rotation.end.to_s(:shortish)} Hero Rotation for Blizzard's Heroes of the Storm."
 		@head[:title] = "Rotation for #{@rotation.start.to_s(:shortish)} - #{@rotation.end.to_s(:shortish)}"
 
-		@rosters = @rotation.rosters.joins(:hero).order('heros.name ASC')
+		@rosters = @rotation.rosters.includes(:hero).order('heros.name ASC')
 		@newest_hero = @rotation.heroes.newest
 	end
 
