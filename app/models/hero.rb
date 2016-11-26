8dcd8a23bd493c3ecfd6f17392398812b247e607
GGCrew <R_Connelie@hotmail.com>
2016-11-26 03:57:17 -0800

    Add date validation to Roster fixture data to prevent a "not gonna happen in real life" testing error

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -137,10 +137,12 @@ class Hero < ActiveRecord::Base
 		weeks = []
 		for hero in self.distinct_heroes
 			if hero.first_rotation
-				#logger.debug("hero.first_rotation.start: #{hero.first_rotation.start} - hero.release_date: #{hero.release_date}")
 				difference = hero.first_rotation.start - hero.release_date
 				difference = (difference / 1.week).to_i
-				#logger.debug("difference: #{difference}")
+				if difference < 0
+					logger.debug("difference: #{difference}")
+					logger.debug("hero.first_rotation.start: #{hero.first_rotation.start}, hero.release_date: #{hero.release_date}")
+				end
 				weeks[difference] ||= 0 # Initialize if not already set
 				weeks[difference] += 1
 			end
