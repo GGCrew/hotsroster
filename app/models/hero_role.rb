d216dc2ff50b65b10b3e2f34e0c10468dca85cc9
GGCrew <R_Connelie@hotmail.com>
2016-11-27 21:27:57 -0800

    Add validations and tests for HeroRole

--- app/models/hero_role.rb
+++ app/models/hero_role.rb
@@ -3,4 +3,9 @@ class HeroRole < ActiveRecord::Base
 	belongs_to	:hero
 	belongs_to	:role
 
+	#..#
+
+	validates :hero, :role, presence: true
+	validates :hero, uniqueness: { scope: :role, message: 'and Role have already been paired.' }
+
 end
