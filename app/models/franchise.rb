6e9cba35659f5c585834fb1a43fb52898ae75d65
GGCrew <R_Connelie@hotmail.com>
2016-12-10 00:39:11 -0800

    Update Franchise#import_from_blizzard to check all sources (US and EU)

--- app/models/franchise.rb
+++ app/models/franchise.rb
@@ -9,30 +9,31 @@ class Franchise < ActiveRecord::Base
 	#..#
 
 	def self.import_from_blizzard
-		address = SOURCE_URLS[:heroes][:us]
-		url = URI.parse(address)
-		html = Net::HTTP.get(url) # TODO: error handling
-		page = Nokogiri::HTML(html)
+		SOURCE_URLS[:heroes].each do |country, address|
+			url = URI.parse(address)
+			html = Net::HTTP.get(url) # TODO: error handling
+			page = Nokogiri::HTML(html)
 
-		#<input type="checkbox" id="warcraft-check" data-ng-model="gameFilters.warcraft" data-ng-change="filterChampions()" />
-		#<label for="warcraft-check" title="Warcraft">
-		#	<i class="heroes-icon-universe-warcraft hero-filter-icon" data-ng-class="{checked: gameFilters.warcraft}"></i>
-		#	<span>Warcraft</span>
-		#</label>
-		universe_options = page.css('#warcraft-check').first.parent.parent.css('label')
-		for option in universe_options
-			name = option.attributes['title'].value	# eg Warcraft
-			value = option.attributes['for'].value	# eg warcraft-check
+			#<input type="checkbox" id="warcraft-check" data-ng-model="gameFilters.warcraft" data-ng-change="filterChampions()" />
+			#<label for="warcraft-check" title="Warcraft">
+			#	<i class="heroes-icon-universe-warcraft hero-filter-icon" data-ng-class="{checked: gameFilters.warcraft}"></i>
+			#	<span>Warcraft</span>
+			#</label>
+			universe_options = page.css('#warcraft-check').first.parent.parent.css('label')
+			for option in universe_options
+				name = option.attributes['title'].value	# eg Warcraft
+				value = option.attributes['for'].value	# eg warcraft-check
 
-			name.strip!
-			value.strip!
-			value.gsub!('-check', '')
+				name.strip!
+				value.strip!
+				value.gsub!('-check', '')
 
-			franchise = self.find_by(value: value)
-			if franchise
-				franchise.update_attributes!(name: name)
-			else
-				franchise = self.find_or_create_by!(value: value, name: name)
+				franchise = self.find_by(value: value)
+				if franchise
+					franchise.update_attributes!(name: name)
+				else
+					franchise = self.find_or_create_by!(value: value, name: name)
+				end
 			end
 		end
 
