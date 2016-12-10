d0c58e5a14d13d19fb80ca81b2bf5345705dc5f6
GGCrew <R_Connelie@hotmail.com>
2016-12-09 20:04:28 -0800

    Update URLs to use HTTPS

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,20 +2,20 @@ GAME_LAUNCH_DATE = DateTime.parse('2015-06-02')
 
 SOURCE_URLS = {
 	heroes: {
-		us: 'http://us.battle.net/heroes/en/heroes/',
-		eu: 'http://eu.battle.net/heroes/en/heroes/'
+		us: 'https://us.battle.net/heroes/en/heroes/',
+		eu: 'https://eu.battle.net/heroes/en/heroes/'
 	},
 	rotations: {
-		us: 'http://us.battle.net/forums/en/heroes/topic/17936383460',
-		eu: 'http://eu.battle.net/forums/en/heroes/topic/13604571130'
+		us: 'https://us.battle.net/forums/en/heroes/topic/17936383460',
+		eu: 'https://eu.battle.net/forums/en/heroes/topic/13604571130'
 	},
 	patch_notes: {
-		us: 'http://us.battle.net/forums/en/heroes/topic/18301004598',
+		us: 'https://us.battle.net/forums/en/heroes/topic/18301004598',
 		eu: nil
 	},
 	sales: {
-		us: 'http://us.battle.net/forums/en/heroes/topic/18183929301',
-		eu: 'http://eu.battle.net/forums/en/heroes/topic/13605640947'
+		us: 'https://us.battle.net/forums/en/heroes/topic/18183929301',
+		eu: 'https://eu.battle.net/forums/en/heroes/topic/13605640947'
 	}
 }
 
