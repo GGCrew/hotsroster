ac65b438cdff33be3be5957d7179b4f70c5932b3
GGCrew <R_Connelie@hotmail.com>
2017-01-07 10:19:56 -0800

    Add missing tests for Franchise

--- test/models/alternate_hero_name_test.rb
+++ test/models/alternate_hero_name_test.rb
@@ -5,6 +5,7 @@ class AlternateHeroNameTest < ActiveSupport::TestCase
 		name: 'Alternate Hero Name'
 	}
 
+	# Validation tests
 	test "should not save alternate hero name without name" do
 		alternate_hero_name = AlternateHeroName.new attributes.reject{|k,v| k == :name}
 		assert_not alternate_hero_name.save
@@ -13,5 +14,21 @@ class AlternateHeroNameTest < ActiveSupport::TestCase
 	test "should save alternate hero name" do
 		alternate_hero_name = AlternateHeroName.new attributes
 		assert alternate_hero_name.save
+	end
+
+	# Scope tests
+	test "should not find any orphans" do
+		assert_empty AlternateHeroName.orphans
+	end
+
+	test "should find orphans" do
+		expected_orphans = []
+		(1..5).each do |index|
+			expected_orphans << AlternateHeroName.create!(name: "Orphan Alternate Hero Name #{index}")
+		end
+
+		assert_not_equal 0, AlternateHeroName.orphans.count
+		assert_equal expected_orphans.length, AlternateHeroName.orphans.length
+		assert_empty expected_orphans - AlternateHeroName.orphans
 	end
 end
