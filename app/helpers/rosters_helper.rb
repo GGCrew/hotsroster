70c1a088035ef700ad8c19778e2aa0a5b7ac198b
GGCrew <R_Connelie@hotmail.com>
2017-03-21 04:34:43 -0700

    Add multi-line formatting when more than 7 heroes in free or restricted rototion sections

--- app/helpers/rosters_helper.rb
+++ app/helpers/rosters_helper.rb
@@ -1,2 +1,22 @@
 module RostersHelper
+
+	def create_roster_groups(roster)
+		groups = []
+
+		roster_count = roster.count
+		if roster_count > 7 # 7 is max amount per row/group
+			# TODO: algorithm for even distribution when more than 2 groups
+
+			break_point = roster_count / 2
+			break_point += 1 if roster_count.odd? # Put "extra" item on top row  -- aesthetics!
+
+			groups << roster[0..(break_point  - 1)]
+			groups << roster[break_point..-1]
+		else
+			groups << roster
+		end
+
+		return groups
+	end
+
 end
