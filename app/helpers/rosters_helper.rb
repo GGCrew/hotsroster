0986414bde7bb75ed9eb55456ebf67fcad16684f
GGCrew <R_Connelie@hotmail.com>
2017-03-23 00:02:01 -0700

    Implent algorithm to even distribute heroes across multiple rows

--- app/helpers/rosters_helper.rb
+++ app/helpers/rosters_helper.rb
@@ -1,9 +1,30 @@
 module RostersHelper
 
 	def create_roster_groups(roster)
-		groups = []
+		max_per_row = 7 # 7 is max amount per row/group
 
 		roster_count = roster.count
+
+		rows_needed = roster_count / max_per_row
+		rows_needed += 1 if ((roster_count % max_per_row) > 0)
+
+		items_per_row = roster_count / rows_needed
+		straggler_count = roster_count % items_per_row
+
+		groups = []
+		roster_start_index = 0
+		roster_end_index = -1
+		(1..rows_needed).each do |row_index|
+			roster_end_index += items_per_row
+			if straggler_count > 0
+				roster_end_index += 1
+				straggler_count -= 1
+			end
+			groups << roster[roster_start_index..roster_end_index]
+			roster_start_index = roster_end_index + 1
+		end
+
+=begin
 		if roster_count > 7 # 7 is max amount per row/group
 			# TODO: algorithm for even distribution when more than 2 groups
 
@@ -15,6 +36,7 @@ module RostersHelper
 		else
 			groups << roster
 		end
+=end
 
 		return groups
 	end
