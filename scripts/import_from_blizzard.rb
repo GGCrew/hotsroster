04e7ba8ba103d28bb4fce73bf4ea9b89352662dd
GGCrew <R_Connelie@hotmail.com>
2017-03-23 00:27:58 -0700

    Update script to use new import methods

--- scripts/import_from_blizzard.rb
+++ scripts/import_from_blizzard.rb
@@ -1,15 +1,15 @@
 action = 'Importing Franchises'
 puts "#{action}..."
-Franchise.import_from_blizzard
+Franchise.import_from_blizzard_hero_pages
 puts "#{action} complete!"
 
 action = 'Importing Heroes'
 puts "#{action}..."
-Hero.import_from_blizzard
+Hero.import_from_blizzard_hero_pages
 puts "#{action} complete!"
 
 action = 'Importing Roster'
 puts "#{action}..."
-Roster.import_from_blizzard
+Roster.import_from_blizzard_hero_pages
 puts "#{action} complete!"
 
