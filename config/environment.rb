2adb7be0da165e98fb584fdb7e4610872ae4e5c5
GGCrew <R_Connelie@hotmail.com>
2017-03-23 17:35:19 -0700

    Specify order of loading specific fixtures to avoid bogus errors when loading the overly-complex rosters fixture

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Specify order to when loading fixtures
+ENV['FIXTURES'] ||= 'heros,date_ranges,rosters' # Roster model tests check for the existence of data in the other two tables
+
