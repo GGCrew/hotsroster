09b53dfeb83e9f2566bd29bed26f8d1280807cd3
GGCrew <R_Connelie@hotmail.com>
2017-05-16 12:35:30 -0700

    Refactor hero image_tags and update them to use Blizzard's new "media.blizzard.com" server

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
 	def format_percentage(value)
 		return number_to_percentage(value, precision: 2)
 	end
+
+	def hero_image_tag(hero)
+		return image_tag("http://media.blizzard.com/heroes/#{hero.slug}/bust.jpg", alt: hero.player_character_name, title: hero.player_character_name)
+	end
 end
