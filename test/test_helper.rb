816a89e98ce508866678db747c015cae47e984ec
GGCrew <R_Connelie@hotmail.com>
2018-07-07 15:48:57 -0700

    Update heroes_json to match current source samples

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,22 +13,43 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   def heroes_json
 =begin
-{"name"=>"Zeratul",
+{"id"=>"Zeratul",
+ "name"=>"Zeratul",
  "title"=>"Dark Prelate",
- "role"=>{"name"=>"Assassin", "slug"=>"assassin"},
+ "baseHeroInfo"=>
+  {"role"=>{},
+   "skins"=>[],
+   "type"=>{},
+   "trait"=>{},
+   "defaultMount"=>{},
+   "abilities"=>[],
+   "heroicAbilities"=>[],
+   "multiclassRoles"=>[],
+   "otherAbilities"=>[],
+   "pairedHeroes"=>[]},
+ "role"=>
+  {"name"=>"Assassin",
+   "slug"=>"assassin",
+   "description"=>"Assassin Slayers of Heroes and bringers of pain."},
  "roleSecondary"=>{},
  "type"=>{"name"=>"Melee", "slug"=>"melee"},
- "stats"=>{"damage"=>0, "utility"=>0, "survivability"=>0, "complexity"=>0},
+ "multiclassRoles"=>[],
+ "pairedHeroes"=>[],
  "slug"=>"zeratul",
- "franchise"=>"starcraft",
- "analyticsName"=>"Zeratul",
- "trait"=>{},
+ "franchise"=>"StarCraft",
+ "stanceSlug"=>"",
+ "difficulty"=>"Hard",
+ "stats"=>{"damage"=>0, "utility"=>0, "survivability"=>0, "complexity"=>0},
  "skins"=>[],
- "abilities"=>[],
- "heroicAbilities"=>[],
+ "releaseDate"=>{"year"=>2014, "month"=>3, "day"=>13},
+ "alternateHeroInfo"=>[],
+ "auxiliaryHeroInfo"=>[],
+ "heroStanceName"=>"",
+ "analyticsName"=>"zeratul",
  "revealed"=>true,
  "inFreeHeroRotation"=>false,
- "freeRotationMinLevel"=>0}
+ "freeRotationMinLevel"=>0,
+ "unified"=>false}
 =end
 
 		heroes_json = []
@@ -41,7 +62,11 @@ class ActiveSupport::TestCase
 				end while secondary_slug == role_slug
 				secondary_name = secondary_slug.titleize
 
-				role_secondary = {"name"=>secondary_name, "slug"=>secondary_slug}
+				role_secondary = {
+          "name"=>secondary_name,
+          "slug"=>secondary_slug,
+          "description"=>"#{secondary_name} Flowery description of secondary role."
+        }
 			else
 				role_secondary = {}
 			end
@@ -54,23 +79,54 @@ class ActiveSupport::TestCase
 			in_free_hero_rotation = (index % 5 == 0)
 			free_rotation_min_level = (in_free_hero_rotation ? [5, 7, 12, 15].sample : 0)
 
+      difficulty = ['Easy', 'Medium', 'Hard', 'Very Hard'].sample
+
+      release_date = {
+        "year"=>(2014..2018).to_a.sample,
+        "month"=>(1..12).to_a.sample,
+        "day"=>(1..28).to_a.sample
+      }
+
 			heroes_json << {
+        "id"=>"Hero #{index}",
 				"name"=>"Hero #{index}",
 				"title"=>"Title for Hero #{index}",
-				"role"=>{"name"=>role_name, "slug"=>role_slug},
+        "baseHeroInfo"=>{
+          "role"=>{},
+          "skins"=>[],
+          "type"=>{},
+          "trait"=>{},
+          "defaultMount"=>{},
+          "abilities"=>[],
+          "heroicAbilities"=>[],
+          "multiclassRoles"=>[],
+          "otherAbilities"=>[],
+          "pairedHeroes"=>[]
+        },
+        "role"=>{
+          "name"=>role_name,
+          "slug"=>role_slug,
+          "description"=>"#{role_name} Flowery description of role."
+        },
 				"roleSecondary"=>role_secondary,
 				"type"=>{"name"=>type_name, "slug"=>type_slug},
-				"stats"=>{"damage"=>0, "utility"=>0, "survivability"=>0, "complexity"=>0},
+        "multiclassRoles"=>[],
+        "pairedHeroes"=>[],
 				"slug"=>"hero-#{index}",
 				"franchise"=>franchise_value,
-				"analyticsName"=>"Hero #{index}",
-				"trait"=>{},
+        "stanceSlug"=>"",
+        "difficulty"=>difficulty,
+				"stats"=>{"damage"=>0, "utility"=>0, "survivability"=>0, "complexity"=>0},
 				"skins"=>[],
-				"abilities"=>[],
-				"heroicAbilities"=>[],
+        "releaseDate"=>release_date,
+        "alternateHeroInfo"=>[],
+        "auxiliaryHeroInfo"=>[],
+        "heroStanceName"=>"",
+				"analyticsName"=>"Hero #{index}".downcase,
 				"revealed"=>true,
 				"inFreeHeroRotation"=>in_free_hero_rotation,
 				"freeRotationMinLevel"=>free_rotation_min_level
+        "unified"=>false
 			}
 		end
 
